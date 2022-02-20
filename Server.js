54ffb80efb0b12950e6923ab2d4ffea2f74a6e62
VimuKale <vimurk111@gmail.com>
2022-02-20 20:30:55 +0530

    added supplylisting route

--- Server.js
+++ Server.js
@@ -50,6 +50,14 @@ app.post("/login", (req, res) => {
                     })
                     .catch(err => res.status(400).json("unable to get User "))
             }
+            if (isValid && (req.body.type === "Admin")) {
+                return db.select('*').from('admindetails')
+                    .where('a_email', '=', req.body.email)
+                    .then(admin => {
+                        res.json(admin[0])
+                    })
+                    .catch(err => res.status(400).json("unable to get User "))
+            }
             else {
                 res.status(400).json('Wrong Credentials')
             }
@@ -118,8 +126,107 @@ app.post('/shelterregister', (req, res) => {
             .catch(trx.rollback)
     }).catch((err) => { console.log(err); res.status(400).json('unable to register') })
 })
-//USER PROFILE------------------------------------------------------------------------------------------------
 
+//ADMIN REGISTER//--------------------------------------------------------------------------------
+app.post('/adminregister', (req, res) => {
+    const { a_name, a_phno, a_email, a_password, a_addr, a_city, a_state, a_zip } = req.body;
+    const hash = bcrypt.hashSync(a_password);
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: a_email,
+            type: "Admin"
+        }).into('login')
+            .then((response) => {
+                return trx('admindetails').insert({
+                    a_name: a_name,
+                    a_phno: a_phno,
+                    a_email: a_email,
+                    a_addr: a_addr,
+                    a_city: a_city,
+                    a_state: a_state,
+                    a_zip: a_zip,
+                    type: "Admin"
+                }).then(admin => {
+                    res.json({
+                        message_type: "success",
+                        message: admin[0]
+                    })
+                })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    }).catch((err) => {
+        console.log(err); res.status(400).json(
+            {
+                message_type: "fail",
+                message: "unable to register admin"
+            }
+        )
+    })
+})
+
+
+
+app.post('/supplylisting', (req, res) => {
+    const { i_id, i_name, i_desc, i_qty, i_cost, s_id, deliver_to, link_to_source, time_frame, status } = req.body;
+
+
+    db('items')
+        .insert({
+            i_id: i_id,
+            i_name: i_name,
+            i_desc: i_desc,
+            i_qty: i_qty,
+            i_cost: i_cost,
+            s_id: s_id,
+            deliver_to: deliver_to,
+            link_to_source: link_to_source,
+            time_frame: time_frame,
+            status: status
+        }).then(i_record => {
+            res.json({
+                message_type: "success",
+                message: i_record[0]
+            })
+        })
+        .catch((err) => {
+            console.log(err); res.status(400).json(
+                {
+                    message_type: "fail",
+                    message: "Unable to register supplies"
+                }
+            )
+        })
+
+})
+
+
+app.get('/items', (req, res) => {
+    db.select('*').from('shelterdetails')
+        .join('items', 'shelterdetails.s_id', 'items.s_id')
+        .then((i_record) => {
+            console.log(i_record[0].i_id);
+            res.json(i_record)
+        })
+        .catch((err) => {
+            console.log(err); res.status(400).json(
+                {
+                    message_type: "fail",
+                    message: "Unable to fetch details"
+                }
+            )
+        })
+})
+
+
+
+
+
+
+
+//USER PROFILE------------------------------------------------------------------------------------------------
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
 
@@ -133,7 +240,6 @@ app.get('/profile/:id', (req, res) => {
         }
     }).catch(err => res.status(400).json('Error while retriving User'))
 })
-
 //--------------------------------------------------------------------------------------
 
 
